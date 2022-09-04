2d2e9cae7533396868975b4aea26a094989818ae
Marcus Ziadé <marcus.ziade@ouraring.com>
2022-09-04 14:39:03 +0300

    Add copy to clipboard and document main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,56 @@
+use clipboard::{ClipboardContext, ClipboardProvider};
 use serde::{Serialize, Deserialize};
 use reqwest::Client;
 
+// The LinksResponse model
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-struct LinksResponse {
+struct SongLinks {
     page_url: String
 }
 
+// Entrypoint of the app. 
+// Gets the user query, and fetches the song.link URL.
 #[tokio::main]
 async fn main() {
+    get_links(&get_user_query()).await;
+}
+
+// Asks the user to paste and confirm a music service URL, formats, and returns the query.
+fn get_user_query() -> String {
     use std::io::{stdin,stdout,Write};
-    let mut search_url = String::new();
+    let mut query = String::new();
     print!("Enter music URL:\n➡️ ");
     let _ = stdout().flush();
-    stdin().read_line(&mut search_url).expect("Did not enter a correct string");
+    stdin().read_line(&mut query).expect("Did not enter a correct string");
+    return query;
+}
 
-    let response = reqwest::get(build_url(&search_url))
-        .await
-        .unwrap();
-    
+// Takes a music service URL as input.
+// checks if the response is succesful, decodes the json,
+// copies the generated song.link URL to the clipboard and prints it to the interface.
+async fn get_links(query: &str) {
+    let response = reqwest::get(build_url(&query))
+    .await
+    .unwrap();
+
     if response.status().is_success() {
         let links = response
-        .json::<LinksResponse>()
+        .json::<SongLinks>()
         .await
         .unwrap();
 
-        println!();
-        println!("Success ✅");
-        println!("{}", links.page_url);
-        println!("Song.link URL copied to your clipboard");
+        let songlink = links.page_url;
+        println!("\nSuccess ✅\n{songlink}\nSong.link URL copied to your clipboard");
+
+        let mut context: ClipboardContext = ClipboardProvider::new().unwrap();
+        context.set_contents(songlink.to_owned()).unwrap();
     } else {
         println!("❌ Error, check your search URL and try again")
     }
 }
 
+// Takes in a music service URL, builds the song.link API query, and returns it as a string.
 fn build_url(search_url: &str) -> String {
     let client = Client::new();
     let request = client
